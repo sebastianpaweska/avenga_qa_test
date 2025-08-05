a78e37f11e01985350fa618b2a11ee67fc4a776e
sebastianpaweska <sebastianpaweska@users.noreply.github.com>
2025-08-05 21:48:56 +0200

    report generation

--- consts/paths.py
+++ consts/paths.py
@@ -8,3 +8,6 @@ class Paths:
     LOG_DIRECTORY = ROOT_DIRECTORY / "logs"
     LOGGING_CONFIG = ROOT_DIRECTORY / "config" / "logging.yaml"
     LOG_FILE = LOG_DIRECTORY / f"{datetime.now().replace(microsecond=0).isoformat().replace(':', '-')}.log"
+    REPORT_DIRECTORY = ROOT_DIRECTORY / "reports"
+    ALLURE_DIRECTORY = ROOT_DIRECTORY / "allure-results"
+    ALLURE_REPORTS_DIRECTORY = ROOT_DIRECTORY / "allure-reports"
--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import pytest
 import logging
 import logging.config
 import yaml
+import shutil
+import time
+import subprocess
+
+from datetime import datetime
+from allure_combine import combine_allure
 
 from consts.paths import Paths
 
@@ -21,6 +27,28 @@ def set_logger():
     logging.config.dictConfig(config)
     return logging.getLogger()
 
+def generate_allure_report():
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    allure_dir = f"{Paths.ALLURE_DIRECTORY}"
+    allure_reports_dir = f"{Paths.ALLURE_REPORTS_DIRECTORY}"
+    report_dir = f"{Paths.REPORT_DIRECTORY}"
+
+    subprocess.run(["allure", "generate", allure_dir, "-o", allure_reports_dir, "--clean"], check=True)
+    combine_allure(allure_reports_dir, report_dir, remove_temp_files=True)
+    report_path = os.path.join(report_dir, "complete.html")
+    test_report_path = os.path.join(report_dir, f"test_report_{timestamp}.html")
+    os.rename(report_path, test_report_path)
+
+def prepare_directories():
+    if os.path.exists(Paths.ALLURE_DIRECTORY):
+        shutil.rmtree(Paths.ALLURE_DIRECTORY)
+    if os.path.exists(Paths.ALLURE_REPORTS_DIRECTORY):
+        shutil.rmtree(Paths.ALLURE_REPORTS_DIRECTORY)
+    os.makedirs(Paths.ALLURE_DIRECTORY, exist_ok=True)
+    os.makedirs(Paths.ALLURE_REPORTS_DIRECTORY, exist_ok=True)
+    os.makedirs(Paths.REPORT_DIRECTORY, exist_ok=True)
+    time.sleep(1)  # allure throws an error if dir is not created, needs short sleep here
+
 @click.group
 def cli():
     pass
@@ -30,10 +58,23 @@ def cli():
 def run(marker):
     logger = set_logger()
     logger.info("Logger initialized")
+
+    prepare_directories()
+
     args = ["-v", "-s", "tests"]
     if marker:
-        args.append(f"-m {marker}")
+        args.append("-m")
+        args.append(marker)
+    args.append(f"--alluredir={Paths.ALLURE_DIRECTORY}")
     retcode = pytest.main(args=args)
+    generate_allure_report()
+
+    # Close all loggers before exit
+    for handler in logging.root.handlers[:]:
+        handler.flush()
+        handler.close()
+        logging.root.removeHandler(handler)
+
     sys.exit(retcode)
 
 
